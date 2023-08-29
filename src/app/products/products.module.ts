800af2f457f1f25d645e6d981257482edc870216
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-08-29 15:48:25 -0400

    form inject

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductPageComponent
+  ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductsModule { }
