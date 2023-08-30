dc2488bc5058abe3c9e44983d970abb65abe6f20
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-08-29 21:24:39 -0400

    mutations

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/users-request.interface';
 
 @Component({
   selector: 'app-properties-page',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
+  public user = signal<User>({
+    id: 7,
+    email: 'michael.lawson@reqres.in',
+    first_name: 'Michael',
+    last_name: 'Lawson',
+    avatar: 'https://reqres.in/img/faces/7-image.jpg'
+  });
+
+  onFieldUpdated( field: keyof User, value: string ) {
+
+    this.user.mutate( current => {
+      switch( field ) {
+        case 'email':
+          current.email = value;
+        break;
+        case 'first_name':
+          current.first_name = value;
+        break;
+        case 'last_name':
+          current.last_name  = value;
+        break;
+      }
+    });
+
+  }
 }
